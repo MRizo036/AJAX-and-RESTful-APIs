cb75e73b9e8b2b556a88db81b429a245e3db16e1
Maria Rizo <mrizo036@west-mec.org>
2019-09-16 17:38:12 -0700

    Error
    Error when tested in browser.

--- 02-AJAX/Exercise-LAMP/www/scripts.js
+++ 02-AJAX/Exercise-LAMP/www/scripts.js
@@ -12,6 +12,20 @@
 // global variables
 var selectedCity = "Tucson, AZ";
 var weatherReport;
+var httpRequest = false;
+
+function getRequestObject(){
+
+   try {
+      httpRequest = new XMLHttpRequest(); 
+   }
+   catch (requestError) {
+       document.querySelector("p.error").innerHTML = "Forecast not supported by your browser.";
+       document.querySelector("p.error").style.display = "block";
+       return false;
+   }
+   return httpRequest;
+}
 
 function getWeather(evt) {
    var latitude;
@@ -32,6 +46,22 @@ function getWeather(evt) {
    } else if (selectedCity === "Montreal, QC") {
       latitude = 45.5601062;
       longitude = -73.7120832;
+   }
+
+   if(!httpRequest){
+      httpRequest = getRequestObject();
+   } ;
+
+   httpRequest.abort();
+
+   httpRequest.open("get", "solar.php?" + "lat=" + latitude + "&lng" + longitude, true);
+   httpRequest.send(null);
+   httpRequest.onreadystatechange = fillWeather;
+}
+
+function fillWeather(){
+   if (httpRequest.readyState === 4 && httpRequest.status === 200){
+      weatherReport = JSON.parse(httpRequest.responseText);
    }
 }
 
